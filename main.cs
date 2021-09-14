873bd3130a326543083726887a2c64720662c76b
Alfonso Sarmiento Ráez <asarmiento@alintia.com>
2021-09-14 11:34:32 +0200

    Third Commit

--- main.cs
+++ main.cs
@@ -4,4 +4,16 @@ namespace Workflow.Specs.Helper
     {
         
 	}	
+	
+	
+	public static class GlassApiHelper2
+    {
+        public static async Task<HttpClient> GetLoggedClient1(string url, string tenant, string user, string password)
+        {
+            var client1 = new HttpClient();
+			var client2 = new HttpClient();
+			var client3 = new HttpClient();
+			var client4 = new HttpClient();
+		}
+	}
 }
